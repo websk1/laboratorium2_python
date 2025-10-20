a2abb37eba5d26eaadbc91b4f139e6f365593a4c
websk1 <kuba13062004@gmail.com>
2025-10-20 14:24:37 +0200

    Ulepszenia

--- laboratorium2.py
+++ laboratorium2.py
@@ -10,31 +10,15 @@ while petla:
         print("Nie wprowadzono żadnych danych.")
     else:
         liczbyCalkowite = [int(liczba) for liczba in dane.split()]
-
         iloscLiczb = len(liczbyCalkowite)
         suma = sum(liczbyCalkowite)
         srednia = suma / iloscLiczb
-
         dodatnie = len([liczba for liczba in liczbyCalkowite if liczba > 0])
         ujemne = len([liczba for liczba in liczbyCalkowite if liczba < 0])
         zera = len([liczba for liczba in liczbyCalkowite if liczba == 0])
-
-
         maks = max(liczbyCalkowite)
         minimum = min(liczbyCalkowite)
-
         odwrotnaKolejnosc = liczbyCalkowite[::-1]
-
-
-        print(f"Liczba wszystkich wartości: {iloscLiczb}")
-        print(f"Suma: {suma}")
-        print(f"Średnia: {srednia:.2f}")
-        print(f"Liczba dodatnich: {dodatnie}")
-        print(f"Liczba ujemnych: {ujemne}")
-        print(f"Liczba zer: {zera}")
-        print(f"Największa wartość: {maks}")
-        print(f"Najmniejsza wartość: {minimum}")
-        print(f"Odwrotna kolejność: {odwrotnaKolejnosc}")
 
         wyniki = (
             f"Liczba wszystkich wartosci: {iloscLiczb}\n"
@@ -53,7 +37,7 @@ while petla:
         with open("wyniki.txt", "w") as f:
             f.write(wyniki)
 
-        #Wykresy
+        # Histogram wartości
         plt.figure()
         plt.hist(liczbyCalkowite, bins=100, edgecolor="black")
         plt.title("Histogram wartości")
@@ -61,7 +45,7 @@ while petla:
         plt.ylabel("Częstość")
         plt.grid(axis="y", linestyle="--", alpha=0.6)
 
-
+        # Wykres słupkowy liczby dodatnich, ujemnych i zer
         plt.figure()
         kategorie = ["Dodatnie", "Ujemne", "Zera"]
         wartosci = [dodatnie, ujemne, zera]
@@ -71,6 +55,7 @@ while petla:
         plt.bar(kategorie, wartosci, color=["green", "red", "gray"])
         plt.title("Liczba dodatnich, ujemnych i zer")
 
+        # Wykres liniowy kolejności wprowadzonych liczb
         plt.figure()
         plt.plot(range(1, iloscLiczb + 1), liczbyCalkowite, marker="o")
         plt.title("Wykres kolejności liczb")
