658e3f9b86996ab5cd27b44f79d496f030d43ef7
websk1 <kuba13062004@gmail.com>
2025-10-20 13:59:19 +0200

    Dodano pętlę

--- laboratorium2.py
+++ laboratorium2.py
@@ -1,62 +1,68 @@
 import matplotlib.pyplot as plt
 
-dane = input("Podaj liczby całkowite oddzielone spacją: ")
+petla = True
 
-if not dane:
-    print("Nie wprowadzono żadnych danych.")
-else:
-    liczbyCalkowite = [int(liczba) for liczba in dane.split()]
+while petla:
 
-    iloscLiczb = len(liczbyCalkowite)
-    suma = sum(liczbyCalkowite)
-    srednia = suma / iloscLiczb
+    dane = input("Podaj liczby całkowite oddzielone spacją: ")
 
-    dodatnie = len([liczba for liczba in liczbyCalkowite if liczba > 0])
-    ujemne = len([liczba for liczba in liczbyCalkowite if liczba < 0])
-    zera = len([liczba for liczba in liczbyCalkowite if liczba == 0])
+    if not dane:
+        print("Nie wprowadzono żadnych danych.")
+    else:
+        liczbyCalkowite = [int(liczba) for liczba in dane.split()]
 
+        iloscLiczb = len(liczbyCalkowite)
+        suma = sum(liczbyCalkowite)
+        srednia = suma / iloscLiczb
 
-    maks = max(liczbyCalkowite)
-    minimum = min(liczbyCalkowite)
-
-    odwrotnaKolejnosc = liczbyCalkowite[::-1]
+        dodatnie = len([liczba for liczba in liczbyCalkowite if liczba > 0])
+        ujemne = len([liczba for liczba in liczbyCalkowite if liczba < 0])
+        zera = len([liczba for liczba in liczbyCalkowite if liczba == 0])
 
 
-    print(f"Liczba wszystkich wartości: {iloscLiczb}")
-    print(f"Suma: {suma}")
-    print(f"Średnia: {srednia:.2f}")
-    print(f"Liczba dodatnich: {dodatnie}")
-    print(f"Liczba ujemnych: {ujemne}")
-    print(f"Liczba zer: {zera}")
-    print(f"Największa wartość: {maks}")
-    print(f"Najmniejsza wartość: {minimum}")
+        maks = max(liczbyCalkowite)
+        minimum = min(liczbyCalkowite)
 
-    print(f"Odwrotna kolejność: {odwrotnaKolejnosc}")
-
-#Wykresy
-    plt.figure()
-    plt.hist(liczbyCalkowite, bins=100, edgecolor="black")
-    plt.title("Histogram wartości")
-    plt.xlabel("Wartość")
-    plt.ylabel("Częstość")
-    plt.grid(axis="y", linestyle="--", alpha=0.6)
+        odwrotnaKolejnosc = liczbyCalkowite[::-1]
 
 
-    plt.figure()
-    kategorie = ["Dodatnie", "Ujemne", "Zera"]
-    wartosci = [dodatnie, ujemne, zera]
-    plt.title("Liczba dodatnich, ujemnych i zer")
-    plt.ylabel("Liczba wystąpień")
-    plt.grid(axis="y", linestyle="--", alpha=0.6)
-    plt.bar(kategorie, wartosci, color=["green", "red", "gray"])
-    plt.title("Liczba dodatnich, ujemnych i zer")
+        print(f"Liczba wszystkich wartości: {iloscLiczb}")
+        print(f"Suma: {suma}")
+        print(f"Średnia: {srednia:.2f}")
+        print(f"Liczba dodatnich: {dodatnie}")
+        print(f"Liczba ujemnych: {ujemne}")
+        print(f"Liczba zer: {zera}")
+        print(f"Największa wartość: {maks}")
+        print(f"Najmniejsza wartość: {minimum}")
+        print(f"Odwrotna kolejność: {odwrotnaKolejnosc}")
 
-    plt.figure()
-    plt.plot(range(1, iloscLiczb + 1), liczbyCalkowite, marker="o")
-    plt.title("Wykres kolejności liczb")
-    plt.xlabel("Indeks")
-    plt.ylabel("Wartość")
-    plt.grid(True)
+    #Wykresy
+        plt.figure()
+        plt.hist(liczbyCalkowite, bins=100, edgecolor="black")
+        plt.title("Histogram wartości")
+        plt.xlabel("Wartość")
+        plt.ylabel("Częstość")
+        plt.grid(axis="y", linestyle="--", alpha=0.6)
 
-    plt.tight_layout()
-    plt.show()
+
+        plt.figure()
+        kategorie = ["Dodatnie", "Ujemne", "Zera"]
+        wartosci = [dodatnie, ujemne, zera]
+        plt.title("Liczba dodatnich, ujemnych i zer")
+        plt.ylabel("Liczba wystąpień")
+        plt.grid(axis="y", linestyle="--", alpha=0.6)
+        plt.bar(kategorie, wartosci, color=["green", "red", "gray"])
+        plt.title("Liczba dodatnich, ujemnych i zer")
+
+        plt.figure()
+        plt.plot(range(1, iloscLiczb + 1), liczbyCalkowite, marker="o")
+        plt.title("Wykres kolejności liczb")
+        plt.xlabel("Indeks")
+        plt.ylabel("Wartość")
+        plt.grid(True)
+
+        plt.tight_layout()
+        plt.show()
+
+        odpowiedz = input("\nWpisz coś żeby wpisać ponownie liczby (pozostaw puste żeby zakończyć): ")
+        petla = odpowiedz or False
